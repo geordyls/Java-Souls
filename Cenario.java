7d94844333adab65311a85237181b455707a6765
geordyls <tegeordy@gmail.com>
2022-05-07 16:07:35 -0300

    0.2

--- Cenario.java
+++ Cenario.java
@@ -1,10 +1,8 @@
 package com.br.javasouls;
 
-
 public class Cenario extends Personagem {
     public int teste;
-    public String combate;
-
+    public int combate;
     public String evento;
 
     public int getTeste() {
@@ -12,29 +10,37 @@ public class Cenario extends Personagem {
     }
 
     public void setTeste(int teste) {
-        if (forca<teste)
+        if (inteligencia>teste)
             System.out.println("Você Morreu");
         else System.out.println("Vitoria!");
     }
 
-    public String getCombate() {
+    public int getCombate() {
         return combate;
     }
 
-    public void setCombate(String combate) {
-        this.combate = combate;
+    public void setCombate(int combate) {
+        if (forca<combate)
+                System.out.println("Se fodeu");
+        else System.out.println("Inimigo Derrotado");
     }
-
     public String getEvento() {
         return evento;
     }
 
     public void setEvento(String evento) {
-        this.evento = evento;
+        if (evento=String)
+            System.out.println("Desafio Conquistado!");
+        else System.out.println("Missão falhou");
     }
 
-
     public static void main(String[] args) {
+        Cenario combate = new Cenario();
+        combate.setCombate(4);
+        Cenario teste = new Cenario();
+                teste.setTeste(1);
+
+
 
     }
 }
